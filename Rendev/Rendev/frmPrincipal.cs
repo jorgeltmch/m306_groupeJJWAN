56a67598bc24f3d02b97ef4e28b2da04c22cc730
Jauch Walter <walter.jch@eduge.ch>
2020-02-24 15:33:38 +0100

    Manuel Utilisateur (incomplet)

--- Rendev/Rendev/frmPrincipal.cs
+++ Rendev/Rendev/frmPrincipal.cs
@@ -16,5 +16,18 @@ namespace Rendev
         {
             InitializeComponent();
         }
+
+        private void AddMap_Load(object sender, EventArgs e)
+        {
+            AddMap.ShowCenter = false;
+            AddMap.MapProvider = GMap.NET.MapProviders.GoogleMapProvider.Instance;
+            GMap.NET.GMaps.Instance.Mode = GMap.NET.AccessMode.ServerOnly;
+            AddMap.Position = new GMap.NET.PointLatLng(46.195566, 6.110237);
+        }
+
+        private void btnAddEvent_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
