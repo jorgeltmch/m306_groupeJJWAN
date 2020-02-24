9cd011d4962b7bb40fea2d248040a51c8ee16d34
NelsonJnrnd <nelson.jnrnd@eduge.ch>
2020-02-24 13:10:15 +0100

    Nelson map first version

---
+++ GreatMap_setup/GreatMap_setup/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GreatMap_setup
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            map.ShowCenter = false;
+            map.MapProvider = GMap.NET.MapProviders.OpenStreetMapProvider.Instance;
+            GMap.NET.GMaps.Instance.Mode = GMap.NET.AccessMode.ServerOnly;
+            map.Position = new GMap.NET.PointLatLng(46.195566, 6.110237);
+        }
+    }
+}
